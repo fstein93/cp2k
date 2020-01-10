e00d2e799caec2f44d3afb2b87a38e971ca96dd8
Frederick Stein <43850145+fstein93@users.noreply.github.com>
2020-01-10 10:20:20 +0100

    Update scripts

---
+++ tools/sympy2fortran/pbe_sr_ab.py
@@ -0,0 +1,523 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 23 17:40:26 2019
+
+@author: Standard
+"""
+
+from sympy import Symbol, Function, pi, sympify, log, exp, sqrt, erf
+from sympy2fortran import create_Routine_from_Function
+from time import time
+
+t_start=time()
+
+# The maximum requested derivative (Hint: the more derivatives requested the more it needs for creating files and the more code will be produced)
+max_deriv=2
+
+# Source file to be created
+file=open('pbe_sr.F', 'w')
+
+# Variables
+x=Symbol('x')
+y=Symbol('y')
+z=Symbol('z')
+
+# Density and its derivative (we need Rho as function for sympy and as symbol for the code)
+zeta=Symbol('zeta')
+rho=Symbol('rho')
+drho=Symbol('drho')
+Rs=Symbol('Rs')
+rhoa=Symbol('rhoa')
+rhob=Symbol('rhob')
+drhoa=Symbol('drhoa')
+drhob=Symbol('drhob')
+
+# Scaling factors
+sx=Symbol('sx')
+sc=Symbol('sc')
+
+zero=sympify(0)
+one=sympify(1)
+two=sympify(2)
+three=sympify(3)
+four=sympify(4)
+five=sympify(5)
+six=sympify(6)
+eight=sympify(8)
+half=sympify('1/2')
+third=sympify('1/3')
+
+# Correlation energy density (e.g. from PW92)
+Ec=Function('Ec')
+
+# mu dependent Correlation energy density
+Ecmu=Function('Ecmu')
+
+# Range-separation parameter
+mu=Symbol('mu')
+
+# Functional specific constants
+gamma=Symbol('gamma')
+gamma_=sympify(0.031091)
+beta_PBE=Symbol('beta_PBE')
+beta_PBE_=sympify(0.066725)
+alpha_C=Symbol('alpha_C')
+alpha_C_=sympify(2.78)
+kappa=Symbol('kappa')
+kappa_=sympify(0.840)
+alpha_X=Symbol('alpha_X')
+alpha_X_=sympify(19.0)
+b_PBE=Symbol('b_PBE')
+b_PBE_=sympify(0.21951)
+c=Symbol('c')
+c_=sympify((one+six*sqrt(three))**half)
+
+# Correlation energy density (e.g. from PW92)
+class Ex(Function):
+    nargs=1
+    name='Ex'
+    
+    @classmethod
+    def eval(cls, rho):
+        return Function('Ex')(rho)
+
+subs_ex=[[Ex, 'calc_Ex', rho]]
+
+# Correlation energy density (e.g. from PW92)
+class Ec(Function):
+    nargs=1
+    name='Ec'
+    
+    @classmethod
+    def eval(cls, rho):
+        return Function('Ec')(rho)
+
+subs_ec=[[Ec, 'calc_Ec', rho]]
+
+# Correlation energy density (e.g. from PW92)
+class Ecab(Function):
+    nargs=2
+    name='Ecab'
+    
+    @classmethod
+    def eval(cls, rho, zeta):
+        return Function('Ecab')(rho, zeta)
+
+subs_ecab=[[Ecab, 'calc_Ecab', rho, zeta]]
+
+# Correlation energy density (e.g. from PW92)
+class Ecmu(Function):
+    nargs=1
+    name='Ecmu'
+    
+    @classmethod
+    def eval(cls, rho):
+        return Function('Ecmu')(rho)
+
+subs_ecmu=[[Ecmu, 'calc_Ecmu', rho]]
+
+# Correlation energy density (e.g. from PW92)
+class Ecmuab(Function):
+    nargs=2
+    name='Ecmuab'
+    
+    @classmethod
+    def eval(cls, rho, zeta):
+        return Function('Ecmuab')(rho, zeta)
+
+subs_ecmuab=[[Ecmuab, 'calc_Ecmuab', rho, zeta]]
+
+# helper routines for exchange part
+
+class kf(Function):
+    nargs=1
+    
+    @classmethod
+    def eval(cls, rho):
+        return (three*pi**two*rho)**third
+
+class s_(Function):
+    nargs=2
+    name='s'
+    
+    @classmethod
+    def eval(cls, rho, drho):
+        return drho/two/kf(rho)/rho
+
+class s(Function):
+    nargs=2
+    name='s'
+    
+    @classmethod
+    def eval(cls, rho, drho):
+        return Function('s')(rho, drho)
+
+subs_s=[[s, 'calc_s', rho, drho]]
+create_Routine_from_Function(s_, file, max_deriv, [], [], [], 'calc_s', [], rho, drho)
+
+class c_1_(Function):
+    nargs=1
+    name='c_1'
+    
+    @classmethod
+    def eval(cls, mu):
+        nu=mu/c
+        return one+sympify(22)*nu**two+sympify(144)*nu**four
+
+class c_1(Function):
+    nargs=1
+    name='a2'
+    
+    @classmethod
+    def eval(cls, mu):
+        return Function('c_1')(mu)
+
+subs_c1=[[c_1, 'calc_c_1', mu]]
+create_Routine_from_Function(c_1_, file, 0, [], [], [], 'calc_c_1', [], mu)
+
+class c_2_(Function):
+    nargs=1
+    name='c_2'
+    
+    @classmethod
+    def eval(cls, mu):
+        nu=mu/c
+        return two*nu**two*(-sympify(7)+sympify(72)*nu**two)
+
+class c_2(Function):
+    nargs=1
+    name='c_2'
+    
+    @classmethod
+    def eval(cls, rs):
+        return Function('c_2')(rs)
+
+subs_c2=[[c_2, 'calc_c_2', mu]]
+create_Routine_from_Function(c_2_, file, 0, [], [], [], 'calc_c_2', [], mu)
+
+class c_3_(Function):
+    nargs=1
+    name='c_3'
+    
+    @classmethod
+    def eval(cls, mu):
+        nu=mu/c
+        return -sympify(864)*nu**four*(two*nu**two-one)
+
+class c_3(Function):
+    nargs=1
+    name='c_3'
+    
+    @classmethod
+    def eval(cls, mu):
+        return Function('c_3')(mu)
+
+subs_c3=[[c_3, 'calc_c_3', mu]]
+create_Routine_from_Function(c_3_, file, 0, [], [], [], 'calc_c_3', [], mu)
+
+class c_4_(Function):
+    nargs=1
+    name='c_4'
+    
+    @classmethod
+    def eval(cls, mu):
+        nu=mu/c
+        return nu**two*(-three-sympify(24)*nu**two+sympify(32)**four+eight*nu*sqrt(pi)*erf(half/nu))
+
+class c_4(Function):
+    nargs=1
+    name='c_4'
+    
+    @classmethod
+    def eval(cls, mu):
+        return Function('c_4')(mu)
+
+subs_c4=[[c_4, 'calc_c_4', mu]]
+create_Routine_from_Function(c_4_, file, 0, [], [], [], 'calc_c_4', [], mu)
+
+class b_T_(Function):
+    nargs=1
+    name='b_T'
+    
+    @classmethod
+    def eval(cls, mu):
+        return (c_2(mu)*exp(one/four/mu**two)-c_1(mu))/(c_3(mu)+sympify(54)*c_4(mu)*exp(one/four/mu**two))
+
+class b_T(Function):
+    nargs=1
+    name='b_T'
+    
+    @classmethod
+    def eval(cls, mu):
+        return Function('b_T')(mu)
+
+subs_bT=[[b_T, 'calc_b_T', mu]]
+create_Routine_from_Function(b_T_, file, 0, [], [], [], 'calc_b_T', [], mu)
+
+class b_(Function):
+    nargs=1
+    name='b'
+    
+    @classmethod
+    def eval(cls, mu):
+        return b_PBE*b_T(mu)/sympify('7/81')*exp(-alpha_X*mu**two)
+
+class b(Function):
+    nargs=1
+    name='b'
+    
+    @classmethod
+    def eval(cls, mu):
+        return Function('b')(mu)
+
+subs_b=[[b, 'calc_b', mu]]
+create_Routine_from_Function(b_, file, 0, [], [], [], 'calc_b', [], mu)
+
+class Fx_(Function):
+    nargs=2
+    name='Fx'
+    
+    @classmethod
+    def eval(cls, rho, drho):
+        return one+kappa-kappa*(one+b(mu)*s(rho, drho)**two/kappa)
+
+class Fx(Function):
+    nargs=2
+    name='a2'
+    
+    @classmethod
+    def eval(cls, rho, drho):
+        return Function('Fx')(rho, drho)
+
+subs_Fx=[[Fx, 'calc_Fx', rho, drho]]
+create_Routine_from_Function(Fx_, file, max_deriv, subs_b+subs_s, [], [], 'calc_Fx', [mu,], rho, drho)
+
+# several helper routines for the correlation part
+
+class ks(Function):
+    nargs=1
+    
+    @classmethod
+    def eval(cls, rho):
+        return sqrt(four*kf(rho)/pi)
+
+class phi_(Function):
+    nargs=1
+    name='phi'
+    
+    @classmethod
+    def eval(cls, zeta):
+        return ((one+zeta)**(two*third)+(one-zeta)**(two*third))/two
+
+class phi(Function):
+    nargs=1
+    name='phi'
+    
+    @classmethod
+    def eval(cls, zeta):
+        return Function('phi')(zeta)
+
+subs_phi=[[phi, 'calc_phi', zeta]]
+create_Routine_from_Function(phi_, file, max_deriv, [], [], [], 'calc_phi', [], zeta)
+
+class t_ab_(Function):
+    nargs=3
+    name='t_ab'
+    
+    @classmethod
+    def eval(cls, rho, zeta, drho):
+        return drho/two/rho/phi(zeta)/ks(rho)
+
+class t_ab(Function):
+    nargs=3
+    name='tab'
+    
+    @classmethod
+    def eval(cls, rho, zeta, drho):
+        return Function('t_ab')(rho, zeta, drho)
+
+subs_tab=[[t_ab, 'calc_t_ab', rho, zeta, drho]]
+create_Routine_from_Function(t_ab_, file, max_deriv, subs_phi, [], [], 'calc_t_ab', [], rho, zeta, drho)
+
+class beta_ab_(Function):
+    nargs=2
+    name='beta_ab'
+    
+    @classmethod
+    def eval(cls, rho, zeta):
+        return beta_PBE*(Ecmuab(rho, zeta)/Ecab(rho, zeta))**alpha_C
+
+class beta_ab(Function):
+    nargs=2
+    name='beta_ab'
+    
+    @classmethod
+    def eval(cls, rho, zeta):
+        return Function('beta_ab')(rho, zeta)
+
+subs_betaab=[[beta_ab, 'calc_beta_ab', rho, zeta]]
+create_Routine_from_Function(beta_ab_, file, max_deriv, subs_ecmuab+subs_ecab, [], [], 'calc_beta_ab', [mu,], rho, zeta)
+
+class A_ab_(Function):
+    nargs=2
+    name='A_ab'
+    
+    @classmethod
+    def eval(cls, rho, zeta):
+        return beta_ab(rho, zeta)/gamma/(exp(-Ecmuab(rho, zeta)/(gamma*phi(zeta)**three))-one)
+
+class A_ab(Function):
+    nargs=2
+    name='A_ab'
+    
+    @classmethod
+    def eval(cls, rho, zeta):
+        return Function('A_ab')(rho, zeta)
+
+subs_Aab=[[A_ab, 'calc_A_ab', rho, zeta]]
+create_Routine_from_Function(A_ab_, file, max_deriv, subs_ecmuab+subs_betaab+subs_phi, [], [], 'calc_A_ab', [mu,], rho, zeta)
+
+class H_ab_(Function):
+    nargs=3
+    name='H_ab'
+    
+    @classmethod
+    def eval(cls, rho, zeta, drho):
+        At2=A_ab(rho, zeta)*t_ab(rho, zeta, drho)**two
+        return gamma*phi(zeta)*log(one+beta_ab(rho, zeta)*t_ab(rho, zeta, drho)**two/gamma*(one+At2)/(one+At2+At2**two))
+
+class H_ab(Function):
+    nargs=3
+    name='H_ab'
+    
+    @classmethod
+    def eval(cls, rho, zeta, drho):
+        return Function('H_ab')(rho, zeta, drho)
+
+subs_Hab=[[H_ab, 'calc_H_ab', rho, zeta, drho]]
+create_Routine_from_Function(H_ab_, file, max_deriv, subs_betaab+subs_tab+subs_Aab+subs_phi, [], [], 'calc_H_ab', [mu,], rho, zeta, drho)
+
+# The actual energy density
+
+# spin-polarized
+class ec_pbe_mu_ab(Function):
+    nargs=3
+    name='lsd_pbe_sr_c'
+    
+    @classmethod
+    def eval(cls, rho, zeta, drho):
+        return Ecmuab(rho, zeta)+H_ab(rho, zeta, drho)
+
+
+# Simplified routines for spin unpolarized systems
+
+class t_(Function):
+    nargs=2
+    name='t'
+    
+    @classmethod
+    def eval(cls, rho, drho):
+        return drho/two/rho/ks(rho)
+
+class t(Function):
+    nargs=2
+    name='t'
+    
+    @classmethod
+    def eval(cls, rho, drho):
+        return Function('t')(rho, drho)
+
+subs_t=[[t, 'calc_t', rho, drho]]
+create_Routine_from_Function(t_, file, max_deriv, [], [], [], 'calc_t', [], rho, drho)
+
+class beta_(Function):
+    nargs=1
+    name='beta'
+    
+    @classmethod
+    def eval(cls, rho):
+        return beta_PBE*(Ecmu(rho)/Ec(rho))**alpha_C
+
+class beta(Function):
+    nargs=1
+    name='beta'
+    
+    @classmethod
+    def eval(cls, rho):
+        return Function('beta')(rho)
+
+subs_beta=[[beta, 'calc_beta', rho]]
+create_Routine_from_Function(beta_, file, max_deriv, subs_ecmu+subs_ec, [], [], 'calc_beta', [mu,], rho)
+
+class A_(Function):
+    nargs=1
+    name='A'
+    
+    @classmethod
+    def eval(cls, rho):
+        return beta(rho)/gamma/(exp(-Ecmu(rho)/gamma)-one)
+
+class A(Function):
+    nargs=1
+    name='A'
+    
+    @classmethod
+    def eval(cls, rho):
+        return Function('a2')(rho)
+
+subs_A=[[A, 'calc_A', rho]]
+create_Routine_from_Function(A_, file, max_deriv, subs_ecmu+subs_beta, [], [], 'calc_A', [mu,], rho)
+
+class H_(Function):
+    nargs=2
+    name='H'
+    
+    @classmethod
+    def eval(cls, rho, drho):
+        At2=A(rho)*t(rho, drho)**two
+        return gamma*log(one+beta(rho)*t(rho, drho)**two/gamma*(one+At2)/(one+At2+At2**two))
+
+class H(Function):
+    nargs=2
+    name='H'
+    
+    @classmethod
+    def eval(cls, rho, drho):
+        return Function('H')(rho, drho)
+
+subs_H=[[H, 'calc_H', rho, drho]]
+create_Routine_from_Function(H_, file, max_deriv, subs_beta+subs_t+subs_A, [], [], 'calc_H', [mu,], rho, drho)
+
+# The actual energy density
+
+# spin-unpolarized
+class ec_pbe_mu(Function):
+    nargs=2
+    name='lda_pbe_sr_c'
+    
+    @classmethod
+    def eval(cls, rho, drho):
+        return Ecmu(rho)+H(rho, drho)
+
+# The actual energy density
+
+# spin-polarized
+class ex_pbe_mu(Function):
+    nargs=2
+    name='pbe_sr_x'
+    
+    @classmethod
+    def eval(cls, rho, drho):
+        return Ex(rho)*Fx(rho, drho)
+
+# List with constants
+constants=[(gamma, gamma_), (beta_PBE, beta_PBE_), (alpha_C, alpha_C_)]
+constants.extend([(b_PBE, b_PBE_), (c, c_), (alpha_X, alpha_X_), (kappa, kappa_)])
+    
+create_Routine_from_Function(ec_pbe_mu_ab, file, max_deriv, subs_ecmuab+subs_Hab, [], [], ec_pbe_mu_ab.name, [mu,], rho, zeta, drho)
+create_Routine_from_Function(ec_pbe_mu, file, max_deriv, subs_ecmu+subs_H, [], [], ec_pbe_mu.name, [mu,], rho, drho)
+create_Routine_from_Function(ex_pbe_mu, file, max_deriv, subs_ex+subs_Fx, [], [], ex_pbe_mu.name, [mu,], rho, drho)
+
+file.close()
+
+print(time()-t_start)
--- tools/sympy2fortran/sympy2fortran.py
+++ tools/sympy2fortran/sympy2fortran.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jul 23 17:40:26 2019
+
 @author: Standard
 """
 
-from sympy import Symbol, cse, fcode, powdenest, sympify
+from sympy import Symbol, cse, fcode, powdenest
 
 '''
 Hints:
@@ -15,43 +16,38 @@ Hints:
     Provide a list with all included constants and, if necessary, a list with 
     additional arguments. These will be included in the created routine.
     
-    This script is very verbose. It shall help the reader to see whether all
-    substitutions work properly. If needed, try additional substitutions or use
-    the 'hard' version: string substitutions. (see the other scripts for 
-    examples.)
+    This script is very verbose. It shall help the user to see whether all
+    substitutions work properly. If necessary, try additional substitutions.
 '''
 
-def create_Routine_from_Function(func, file, max_deriv, list_subs, constants, name, add_args, *args):
-    deriv, symb=get_deriv(func, max_deriv, *args)
+def create_Routine_from_Function(func, file, max_deriv, intermediates, list_subs, constants, name, add_args, *args):
+    deriv=get_deriv(func, max_deriv,True, *args)
     
-    common, deriv=postprocessing(deriv, list_subs, name)
+    common, deriv=postprocessing(deriv, intermediates, list_subs, name, max_deriv)
     
     print('Number of common subexpressions for routine {}: {}'.format(name, len(common)))
     
     arguments=[a for a in args]
     arguments.extend(add_args)
     
-    write_file(common, deriv, symb, constants, name, arguments, file)
+    write_file(common, deriv, func, intermediates, constants, max_deriv, name, len(args), arguments, file)
 
-def get_deriv(func, max_deriv, *args):
+def get_deriv(func, max_deriv, be_verbose=True, *args):
     ''' This function calculates recursively all partial derivatives of a sympy
     function func depending on the sympy variables in list var up to order 
     max_deriv. It assumes continouity of the function and its derivatives up 
-    to the given order and returns symbols for the derivatives 
-    according to (func.name)_(var1)_(var2)... with index(var1)<index(var2)<... .
+    to the given order.
     '''
     
-    print()
-    print('Calculate derivatives of function '+func.name)
+    if be_verbose:
+        print()
+        print('Calculate derivatives of function '+func.name)
     
     # result array with already calculated derivatives
     deriv=[func(*args)]
     
     # Array with last variable w.r.t that we derived lastly to take symmetries into account
     config=[0]
-    
-    # Array with symbols for the fortran file
-    symb=[Symbol(str(func))]
     
     offset=0
     
@@ -65,25 +61,26 @@ def get_deriv(func, max_deriv, *args):
             # Thus, we can take symmetries into account
             for j, arg in enumerate(args[config[i]:]):
                 deriv.append(d.diff(arg))
-                symb.append(Symbol(symb[offset+i].name+'_'+arg.name))
                 new_config.append(i+j)
         config=new_config
         offset=len(deriv)-len(new_config)
     
-    print('Finished calculating derivatives')
-    print()
+    if be_verbose:
+        print('Finished calculating derivatives')
+        print()
     
-    return deriv, symb
+    return deriv
 
-def postprocessing(deriv, list_sub, name):
+def postprocessing(deriv, intermediates, list_subs, name, max_deriv):
     '''
     This function does substitutions and performs a common subexpression 
     elimination (CSE) afterwards. To support the creation of good expressions, 
     the routine is very verbose.
     
     deriv               list of derivatives (sympy expressions)
-    list_sub            list with substitutions
+    intermediates       list with intermediate functions with dependent variables
     name                name of the routine
+    max_deriv           maximum needed derivative
     '''
     
     print('Start postprocessing of function '+name)
@@ -95,14 +92,18 @@ def postprocessing(deriv, list_sub, name):
         print(d)
         
         # Perform substitutions in sympy representation
-        for sub in list_sub:
-            print(sub[0], sub[1])
-            d=d.subs(sub[0], sub[1]).doit()
-            d=powdenest(d, force=True)
+        for func, call, *args in intermediates:
+            derivs=get_deriv(func, max_deriv, False, *args)[::-1]
+            for j, sub in enumerate(derivs):
+                d=d.subs(sub, Symbol(str(func)+'('+str(len(derivs)-j-1)+')')).doit()
+                d=powdenest(d, force=True)
+        
+        for token, sub in list_subs:
+            d=d.subs(token, sub)
             
         # Do some simplifications (hint: simplify is quite expensive and 
         # does strange things from time to time, if it is appropriate for your 
-        # problem, decomment the appropriate line!)
+        # problem, decomment the following line!)
         #d=simplify(d)
         
         # Finally copy the result
@@ -115,7 +116,7 @@ def postprocessing(deriv, list_sub, name):
     # Perform a common subexpression elimination
     return cse(deriv)
     
-def write_file(common, deriv, symb, constants, name, arguments, file):
+def write_file(common, deriv, func, intermediates, constants, max_deriv, name, nargs, arguments, file):
     '''
     This function generates the code to calculate a set of functions in 
     Fortran 2008. It uses the general format from CP2K. It does not include 
@@ -125,13 +126,15 @@ def write_file(common, deriv, symb, constants, name, arguments, file):
     substituted. (Then, you either have to substitute them by hand or find 
     an appropriate substitution.)
     
-    deriv     list of derivatives (sympy expressions)
-    symb      list of symbols for derivatives from get_deriv
-    constants list of constants of format (Symbol, value)
-    list_sub  list with substitutions
-    name      name of the routine
-    arguments list with arguments
-    file      File object where to write everything
+    common          list with common subexpressions
+    deriv           list of derivatives (sympy expressions)
+    func            our function object
+    intermediates   list with intermediate functions
+    constants       list of constants of format (Symbol, value)
+    name            name of the routine
+    nargs           number of arguments of func
+    arguments       list with arguments
+    file            File object where to write everything
     '''
     
     # Get string representation of the arguments
@@ -140,11 +143,6 @@ def write_file(common, deriv, symb, constants, name, arguments, file):
         input_var+=str(s)+', '
     else:
         input_var=input_var[:-2]
-    output_var=''
-    for s in symb:
-        output_var+=str(s)+', '
-    else:
-        output_var=output_var[:-2]
         
     # Get a string of local variables
     local_var=''
@@ -152,31 +150,40 @@ def write_file(common, deriv, symb, constants, name, arguments, file):
         local_var+=str(s)+', '
     for s, d in constants:
         local_var+=str(s)+', '
+    for f, call, *args in intermediates:
+        local_var+=str(f)+'(0:'+str(len(get_deriv(f, max_deriv, False, *args))-1)+'), '
     else:
         local_var=local_var[:-2]
         
     # List with all needed expressions
     expr=constants
     expr.extend(common)
-    expr.extend([(symb[i], func) for i, func in enumerate(deriv)])
+    expr.extend([(Symbol(str(func)+'('+str(i)+')'), f) for i, f in enumerate(deriv)])
     
     print('Write file')
     
     # Write out everything
     file.write('\n')
-    file.write(('   SUBROUTINE '+name+'('+input_var+', '+output_var+')\n').replace('_,', ',').replace('__', '_'))
+    file.write(('   PURE SUBROUTINE '+name+'('+input_var+', '+str(func)+', max_deriv)\n').replace('_,', ',').replace('__', '_'))
     file.write(('      REAL(KIND=dp), INTENT(IN) :: '+input_var+'\n').replace('_,', ',').replace('__', '_'))
-    file.write(('      REAL(KIND=dp), INTENT(INOUT) :: '+output_var+'\n').replace('_,', ',').replace('__', '_'))
+    file.write('      INTEGER, INTENT(IN) :: max_deriv\n')
+    file.write(('      REAL(KIND=dp), INTENT(OUT) :: '+str(func)+'(0:)\n').replace('_(', '(').replace('_,', ',').replace('__', '_'))
     file.write('\n')
     # character 34 is '"'
     file.write('      CHARACTER(LEN=*), PARAMETER :: routineN = '+chr(34)+name+chr(34)+', routineP = moduleN//'+chr(34)+':'+chr(34)+'//routineN\n')
     file.write('\n')
     file.write('      REAL(KIND=dp) :: '+local_var+'\n')
     file.write('\n')
+    for s, call, *args in intermediates:
+        inp_var=''
+        for i in args:
+            inp_var+=str(i)+', '
+        inp_var+=str(s)+', max_deriv'
+        file.write('      CALL '+call+'('+inp_var+')\n')
+    file.write('\n')
     for s, func in expr:
-        file.write('      '+fcode(func, assign_to=s, source_format='free', standard=2008).replace('_ ', ' ').replace('__', '_').replace('d0', '_dp').replace('1.0/', '1.0_dp/').replace(' 1/', '1.0_dp/').replace('parameter (pi = 3.14159265358979_dp)\n', '').replace('parameter (pi = 3.1415926535897932_dp)\n', '')+'\n')
+        file.write('      '+fcode(func, assign_to=s, source_format='free', standard=2008).replace('_(', '(').replace('_ ', ' ').replace('__', '_').replace('d0', '_dp').replace('1.0/', '1.0_dp/').replace(' 1/', ' 1.0_dp/').replace('parameter (pi = 3.14159265358979_dp)\n', '').replace('parameter (pi = 3.1415926535897932_dp)\n', '')+'\n')
     file.write('\n')
     file.write('   END SUBROUTINE '+name+'\n')
     
     print('Finished writing function '+name)
-
