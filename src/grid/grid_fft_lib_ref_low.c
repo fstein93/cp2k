77c970de0bbb2a312e4584673a787f2b9ecfa4f7
Frederick Stein <fredericks@t-online.de>
2025-05-07 18:40:03 +0200

    Split indices in small and large components

--- src/grid/grid_fft_lib_ref_low.c
+++ src/grid/grid_fft_lib_ref_low.c
@@ -22,43 +22,95 @@ void fft_ref_1d_fw_local_low(const double complex *grid_in,
                              const int number_of_ffts, const int stride_in,
                              const int stride_out, const int distance_in,
                              const int distance_out) {
+
+  // Determine a factorization of the FFT size
+  int small_factor = 1;
+  for (int i = 2; i * i <= fft_size; i++) {
+    if (fft_size % i == 0) {
+      small_factor = i;
+      break;
+    }
+  }
+  const int large_factor = fft_size / small_factor;
+
   const double pi = acos(-1.0);
-#pragma omp parallel for default(none) collapse(2)                             \
-    shared(grid_in, grid_out, fft_size, number_of_ffts, pi, stride_in,         \
-               stride_out, distance_in, distance_out)
+#pragma omp parallel for default(none) collapse(2) shared(                     \
+        grid_in, grid_out, fft_size, number_of_ffts, pi, stride_in,            \
+            stride_out, distance_in, distance_out, small_factor, large_factor)
   for (int fft = 0; fft < number_of_ffts; fft++) {
-    for (int index_out = 0; index_out < fft_size; index_out++) {
-      double complex tmp = 0.0;
-      for (int index_in = 0; index_in < fft_size; index_in++) {
-        tmp += grid_in[index_in * stride_in + fft * distance_in] *
-               cexp(-2.0 * I * pi * index_out * index_in / fft_size);
+    for (int index_out_small = 0; index_out_small < small_factor;
+         index_out_small++) {
+      for (int index_out_large = 0; index_out_large < large_factor;
+           index_out_large++) {
+        double complex tmp = 0.0;
+        for (int index_in_large = 0; index_in_large < large_factor;
+             index_in_large++) {
+          for (int index_in_small = 0; index_in_small < small_factor;
+               index_in_small++) {
+            tmp += grid_in[(index_in_small * large_factor + index_in_large) *
+                               stride_in +
+                           fft * distance_in] *
+                   cexp(-2.0 * I * pi *
+                        (index_out_large * small_factor + index_out_small) *
+                        (index_in_small * large_factor + index_in_large) /
+                        fft_size);
+          }
+        }
+        grid_out[fft * distance_out +
+                 (index_out_large * small_factor + index_out_small) *
+                     stride_out] = tmp;
       }
-      grid_out[fft * distance_out + index_out * stride_out] = tmp;
     }
   }
 }
 
 /*******************************************************************************
- * \brief Naive implementation of backwards FFT to transposed format (for easier
- *transposition). \author Frederick Stein
+ * \brief Naive implementation of backwards FFT to transposed format (for
+ *easier transposition). \author Frederick Stein
  ******************************************************************************/
 void fft_ref_1d_bw_local_low(const double complex *grid_in,
                              double complex *grid_out, const int fft_size,
                              const int number_of_ffts, const int stride_in,
                              const int stride_out, const int distance_in,
                              const int distance_out) {
+
+  // Determine a factorization of the FFT size
+  int small_factor = 1;
+  for (int i = 2; i * i <= fft_size; i++) {
+    if (fft_size % i == 0) {
+      small_factor = i;
+      break;
+    }
+  }
+  const int large_factor = fft_size / small_factor;
+
   const double pi = acos(-1.0);
-#pragma omp parallel for default(none) collapse(2)                             \
-    shared(grid_in, grid_out, fft_size, number_of_ffts, pi, stride_in,         \
-               stride_out, distance_in, distance_out)
+#pragma omp parallel for default(none) collapse(2) shared(                     \
+        grid_in, grid_out, fft_size, number_of_ffts, pi, stride_in,            \
+            stride_out, distance_in, distance_out, small_factor, large_factor)
   for (int fft = 0; fft < number_of_ffts; fft++) {
-    for (int index_out = 0; index_out < fft_size; index_out++) {
-      double complex tmp = 0.0;
-      for (int index_in = 0; index_in < fft_size; index_in++) {
-        tmp += grid_in[index_in * stride_in + fft * distance_in] *
-               cexp(2.0 * I * pi * index_out * index_in / fft_size);
+    for (int index_out_small = 0; index_out_small < small_factor;
+         index_out_small++) {
+      for (int index_out_large = 0; index_out_large < large_factor;
+           index_out_large++) {
+        double complex tmp = 0.0;
+        for (int index_in_large = 0; index_in_large < large_factor;
+             index_in_large++) {
+          for (int index_in_small = 0; index_in_small < small_factor;
+               index_in_small++) {
+            tmp += grid_in[(index_in_small * large_factor + index_in_large) *
+                               stride_in +
+                           fft * distance_in] *
+                   cexp(2.0 * I * pi *
+                        (index_out_large * small_factor + index_out_small) *
+                        (index_in_small * large_factor + index_in_large) /
+                        fft_size);
+          }
+        }
+        grid_out[fft * distance_out +
+                 (index_out_large * small_factor + index_out_small) *
+                     stride_out] = tmp;
       }
-      grid_out[fft * distance_out + index_out * stride_out] = tmp;
     }
   }
 }
