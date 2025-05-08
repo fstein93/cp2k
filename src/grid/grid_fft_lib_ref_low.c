2f5859f860636cb6c9898e15ddb5b5cb2a10c62d
Frederick Stein <fredericks@t-online.de>
2025-05-08 08:41:31 +0200

    Split entry point from actual routine used for recursion
    This prevents transposition between smaller FFTs

--- src/grid/grid_fft_lib_ref_low.c
+++ src/grid/grid_fft_lib_ref_low.c
@@ -13,6 +13,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+// We need these definitions for the recursion within this module
+void fft_ref_1d_fw_local_internal(double complex *grid_in,
+                                  double complex *grid_out, const int fft_size,
+                                  const int number_of_ffts);
+void fft_ref_1d_bw_local_internal(double complex *grid_in,
+                                  double complex *grid_out, const int fft_size,
+                                  const int number_of_ffts);
+
 /*******************************************************************************
  * \brief Naive implementation of FFT.
  * \author Frederick Stein
@@ -57,6 +65,56 @@ void fft_ref_1d_bw_local_naive(const double complex *grid_in,
             grid_in[fft + index_in * number_of_ffts] * phase_factor;
       }
     }
+  }
+}
+
+/*******************************************************************************
+ * \brief Naive implementation of FFT from transposed format (for easier
+ *transposition). \author Frederick Stein
+ ******************************************************************************/
+void fft_ref_1d_fw_local_internal(double complex *grid_in,
+                                  double complex *grid_out, const int fft_size,
+                                  const int number_of_ffts) {
+
+  // Determine a factorization of the FFT size
+  int small_factor = 1;
+  for (int i = 2; i * i <= fft_size; i++) {
+    if (fft_size % i == 0) {
+      small_factor = i;
+      break;
+    }
+  }
+  const int large_factor = fft_size / small_factor;
+  assert(small_factor * large_factor == fft_size);
+  assert(small_factor <= large_factor);
+
+  const double pi = acos(-1.0);
+  if (small_factor == 1 || large_factor == 1) {
+    // If the FFT size is prime, we can use the naive implementation
+    fft_ref_1d_fw_local_naive(grid_in, grid_out, fft_size, number_of_ffts);
+  } else {
+    // Perform FFTs along the shorter sub-dimension
+    fft_ref_1d_fw_local_naive(grid_in, grid_out, small_factor,
+                              number_of_ffts * large_factor);
+// Transpose and multiply with twiddle factors
+#pragma omp parallel for default(none) collapse(2)                             \
+    shared(grid_in, grid_out, fft_size, number_of_ffts, pi, small_factor,      \
+               large_factor)
+    for (int index_small = 0; index_small < small_factor; index_small++) {
+      for (int index_large = 0; index_large < large_factor; index_large++) {
+        const double complex phase_factor =
+            cexp(-2.0 * I * pi * index_small * index_large / fft_size);
+        for (int fft = 0; fft < number_of_ffts; fft++) {
+          grid_in[fft +
+                  (index_large * small_factor + index_small) * number_of_ffts] =
+              grid_out[fft + (index_small * large_factor + index_large) *
+                                 number_of_ffts] *
+              phase_factor;
+        }
+      }
+    }
+    fft_ref_1d_fw_local_internal(grid_in, grid_out, large_factor,
+                                 number_of_ffts * small_factor);
   }
 }
 
@@ -120,9 +178,8 @@ void fft_ref_1d_fw_local_low(double complex *grid_in, double complex *grid_out,
         }
       }
     }
-    fft_ref_1d_fw_local_low(
-        grid_out, grid_in, large_factor, number_of_ffts * small_factor,
-        number_of_ffts * small_factor, number_of_ffts * small_factor, 1, 1);
+    fft_ref_1d_fw_local_internal(grid_out, grid_in, large_factor,
+                                 number_of_ffts * small_factor);
   }
 // Reorder to the requested output format
 #pragma omp parallel for default(none) collapse(3)                             \
@@ -137,6 +194,56 @@ void fft_ref_1d_fw_local_low(double complex *grid_in, double complex *grid_out,
                               number_of_ffts];
       }
     }
+  }
+}
+
+/*******************************************************************************
+ * \brief Cooley-Tukey step (For internal use only, special format).
+ * \author Frederick Stein
+ ******************************************************************************/
+void fft_ref_1d_bw_local_internal(double complex *grid_in,
+                                  double complex *grid_out, const int fft_size,
+                                  const int number_of_ffts) {
+
+  // Determine a factorization of the FFT size
+  int small_factor = 1;
+  for (int i = 2; i * i <= fft_size; i++) {
+    if (fft_size % i == 0) {
+      small_factor = i;
+      break;
+    }
+  }
+  const int large_factor = fft_size / small_factor;
+  assert(small_factor * large_factor == fft_size);
+  assert(small_factor <= large_factor);
+
+  const double pi = acos(-1.0);
+  if (small_factor == 1 || large_factor == 1) {
+    // If the FFT size is prime, we can use the naive implementation
+    fft_ref_1d_bw_local_naive(grid_in, grid_out, fft_size, number_of_ffts);
+  } else {
+    // Perform FFTs along the shorter sub-dimension
+    fft_ref_1d_bw_local_naive(grid_in, grid_out, small_factor,
+                              number_of_ffts * large_factor);
+// Transpose and multiply with twiddle factors
+#pragma omp parallel for default(none) collapse(2)                             \
+    shared(grid_in, grid_out, fft_size, number_of_ffts, pi, small_factor,      \
+               large_factor)
+    for (int index_small = 0; index_small < small_factor; index_small++) {
+      for (int index_large = 0; index_large < large_factor; index_large++) {
+        const double complex phase_factor =
+            cexp(2.0 * I * pi * index_small * index_large / fft_size);
+        for (int fft = 0; fft < number_of_ffts; fft++) {
+          grid_in[fft +
+                  (index_large * small_factor + index_small) * number_of_ffts] =
+              grid_out[fft + (index_small * large_factor + index_large) *
+                                 number_of_ffts] *
+              phase_factor;
+        }
+      }
+    }
+    fft_ref_1d_bw_local_internal(grid_in, grid_out, large_factor,
+                                 number_of_ffts * small_factor);
   }
 }
 
@@ -200,9 +307,8 @@ void fft_ref_1d_bw_local_low(double complex *grid_in, double complex *grid_out,
         }
       }
     }
-    fft_ref_1d_bw_local_low(
-        grid_out, grid_in, large_factor, number_of_ffts * small_factor,
-        number_of_ffts * small_factor, number_of_ffts * small_factor, 1, 1);
+    fft_ref_1d_bw_local_internal(grid_out, grid_in, large_factor,
+                                 number_of_ffts * small_factor);
   }
 // Reorder to the requested output format
 #pragma omp parallel for default(none) collapse(3)                             \
