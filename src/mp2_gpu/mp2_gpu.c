b9d9ab02c641a048138dd96b81a247f94f9f9464
ADOBApps <acxelorozco@gmail.com>
2026-08-07 02:37:04 +0200

    fix errors intent 25 BIb_C_L_size fix

--- src/mp2_gpu/mp2_gpu.c
+++ src/mp2_gpu/mp2_gpu.c
@@ -879,9 +879,8 @@ void calc_ri_mp2_energy(
     }
 
     double* replicated_BIb_C = c_replicate_iaK_2intgroup(
-        // (double**)&BIb_C,
         BIb_C,
-        my_group_L_size,
+        dimen_RI,
         comm_exchange_out,
         comm_rep_out,
         homo,
