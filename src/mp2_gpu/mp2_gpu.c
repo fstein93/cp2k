28b11eb248947456748fc96c169de2a0dbf829b0
ADOBApps <acxelorozco@gmail.com>
2026-08-12 12:00:07 +0200

    calc_ex section assert test 2

--- src/mp2_gpu/mp2_gpu.c
+++ src/mp2_gpu/mp2_gpu.c
@@ -1315,8 +1315,8 @@ void calc_ri_mp2_energy(
                         }
                     }
                     if (calc_ex) {
-                        for (int b = 0; b < my_B_size; b++) {
-                            assert((b + 1) == my_B_size);
+                        for (int b = 0; b <= my_B_size; b++) {
+                            // assert((b + 1) == my_B_size);
                             int b_global = b + my_B_virtual_start - 1;
                             for (int a = 0; a < my_B_size; a++) {
                                 int a_global = a + my_B_virtual_start - 1;
